7e4b4a7e2c28f34b0acd28b776f8957c58f63983
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-01-28 16:29:08 -0300

    Primer commit

---
+++ projects/00-hola-mundo/src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+//---------------------------------------------------------------
+
+createRoot(document.getElementById("root")).render(
+  <React.Fragment>
+    <button>Hola boton</button>
+    <button>Hola boton</button>
+  </React.Fragment>
+);

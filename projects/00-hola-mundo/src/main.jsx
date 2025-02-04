3aae36650be735fef92a612f5935c978f55f952a
EmilioRamirez <emi.emilioramirez@gmail.com>
2025-02-04 17:04:04 -0300

    Update project

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -3,9 +3,4 @@ import { createRoot } from "react-dom/client";
 
 //---------------------------------------------------------------
 
-createRoot(document.getElementById("root")).render(
-  <React.Fragment>
-    <button>Hola boton</button>
-    <button>Hola boton</button>
-  </React.Fragment>
-);
+createRoot(document.getElementById("root")).render(<div>Twitter Card</div>);
